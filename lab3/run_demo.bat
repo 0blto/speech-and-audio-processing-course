01ca61117305e1677f756f99cef0227d8d07d09b
EgorChernov5 <EgorChernov3140@gmail.com>
2026-05-22 10:19:55 +0300

    Update

--- lab3/run_demo.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python check_env.py || exit /b 1
-python scripts\inference_compare.py
-pause
--- lab3/scripts/speedupaudio.py
+++ lab3/scripts/speedupaudio.py
@@ -1,7 +1,7 @@
 from pydub import AudioSegment
 from pydub.playback import play
 
-audio = AudioSegment.from_file("C:/Workspace/speech/lab3/outputs/finetuned_vits/vits_vctk_A_20260424_175444-April-24-2026_05+54PM-cae8682_synthesis.wav")
+audio = AudioSegment.from_file(r"vits_vctk_A_20260424_175444_April_24_2026_05+54PM_cae8682_synthesis.wav")
 
 faster_audio = audio._spawn(audio.raw_data, overrides={
     "frame_rate": int(audio.frame_rate * 2.2)
